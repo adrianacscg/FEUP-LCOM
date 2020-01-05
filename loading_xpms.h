2ef12eaf77015a43ddf4077ed576f85e3b5533bc
Beatriz Mendes <45185351+biam05@users.noreply.github.com>
2020-01-05 15:00:43 +0000

    Add files via upload

--- loading_xpms.h
+++ loading_xpms.h
@@ -4,14 +4,110 @@
 #include "types.h"
 #include "rtc.h"
 
-int loading_xpms();
+
+/** @defgroup Functions loading_xpms.c
+ * @{
+ *
+ * Functions used to help working with the xpms
+ */
+
+
+/**
+ * @brief Loads all the xpms
+ * 
+ * @return Return 0 upon success
+ */
+int loading_xpms();     
+
+/**
+ * @brief Decides in what position the rodolph should be in (used to change rudolph's eyes)
+ * 
+ * Depends on the mouse postion
+ * 
+ * @param pos_x position x of the mouse
+ * @param pos_y position y of the mouse
+ * 
+ * @return the xpm_image_t of the correspondent rudolph position
+ */
 xpm_image_t decide_rudolph(uint16_t pos_x, uint16_t pos_y);
+
+/**
+ * @brief Decides in what position the sleep bar should be in
+ * 
+ * The sleep bar changes with time
+ * Depends on time and if the character is sleeping or not
+ * 
+ * @param sb state of the current sleep bar
+ * 
+ * @return the xpm_image_t of the correspondent sleep bar
+ */
 xpm_image_t decide_sleep_bar(enum sleep_bar sb);
+
+
+/**
+ * @brief Decides in what position the fun bar should be in
+ * 
+ * The fun bar changes with time
+ * Depends on time and if the character is playing
+ * 
+ * @param sb state of the current play bar
+ * 
+ * @return the xpm_image_t of the correspondent play bar
+ */
 xpm_image_t decide_play_bar(enum play_bar pb);
+
+/**
+ * @brief Decides in what position the eat bar should be in
+ * 
+ * The eat bar changes with time
+ * Depends on time and if the character is eating
+ * 
+ * @param sb state of the current eat bar
+ * 
+ * @return the xpm_image_t of the correspondent eat bar
+ */
 xpm_image_t decide_food_bar(enum food_bar fb);
+
+/**
+ * @brief Decides the value of the counter of the minigame
+ * 
+ * The counter changes with time
+ * Depends on time
+ * 
+ * @param c current time
+ * 
+ * @return the xpm_image_t of the correspondent counter value
+ */
 xpm_image_t decide_time(enum counter_bar c);
+
+/**
+ * @brief Decides the value of dozens number of the scores
+ * 
+ * This value changes when the character presses the correct key 10 times
+ * 
+ * @return enum value of the dozens number for the score
+ */
 enum score_bar_2 event_2();
+
+/**
+ * @brief Decides the value of unities number of the scores
+ * 
+ * This value changes when the character presses the correct key 1 time
+ * 
+ * @return enum value of the unities number for the score
+ */
 enum score_bar_1 event_1();
+
+/**
+ * @brief Decides the value of the counter of the minigame
+ * 
+ * The counter changes with time
+ * Depends on time
+ * 
+ * @param c current time
+ * 
+ * @return the xpm_image_t of the correspondent counter value
+ */
 xpm_image_t decide_score_2_game();
 xpm_image_t decide_score_2();
 xpm_image_t decide_score_1_game();
--- macros_proj.h
+++ macros_proj.h
@@ -3,63 +3,74 @@
 
 #include <stdint.h>
 
-#define CHAR_X                324
-#define CHAR_Y                313
+/** @defgroup Project Macros + RTC Macros 
+ * @{
+ *
+ * Constants used in the project (Positions) + Macros of the RTC
+ */
 
-#define MIN_ESQUERDA_X        0
-#define MAX_ESQUERDA_X        341
-#define MIN_MEIO_X            341 
-#define MAX_MEIO_X            682
-#define MIN_DIREITA_X         682
-#define MAX_DIREITA_X         1024
-#define MIN_CIMA_Y            0
-#define MAX_CIMA_Y            384
-#define MIN_BAIXO_Y           384
-#define MAX_BAIXO_Y           768
+#define CHAR_X                324 /**< @brief character position (x) */
+#define CHAR_Y                313 /**< @brief character position (y) */
 
-#define FOOD_POS_X            820
-#define FOOD_POS_Y            550
+/* Screen Divisions */
 
-#define CHOOSE_RUDOLPH_MIN_X  350
-#define CHOOSE_RUDOLPH_MAX_X  675
-#define CHOOSE_RUDOLPH_MIN_Y  235
-#define CHOOSE_RUDOLPH_MAX_Y  535
+#define MIN_ESQUERDA_X        0   /**< @brief minimum value of the left rectangle of the screen (x) */
+#define MAX_ESQUERDA_X        341 /**< @brief minimum value of the left rectangle of the screen (x) */ */
+#define MIN_MEIO_X            341 /**< @brief minimum value of the center rectangle of the screen (x)  */
+#define MAX_MEIO_X            682 /**< @brief maximum value of the center rectangle of the screen (x)  */
+#define MIN_DIREITA_X         682 /**< @brief minimum value of the right rectangle of the screen (x)  */
+#define MAX_DIREITA_X         1024  /**< @brief maximum value of the right rectangle of the screen (x)  */
+#define MIN_CIMA_Y            0   /**< @brief minimum value of the top rectangle of the screen (y) */
+#define MAX_CIMA_Y            384 /**< @brief maximum value of the top rectangle of the screen (y) */
+#define MIN_BAIXO_Y           384 /**< @brief minimum value of the bottom rectangle of the screen (y) */
+#define MAX_BAIXO_Y           768 /**< @brief maximum value of the bottom rectangle of the screen (y) */
 
-#define SLEEP_MIN_X           259
-#define SLEEP_MAX_X           331
-#define SLEEP_MIN_Y           672
-#define SLEEP_MAX_Y           742
+#define FOOD_POS_X            820 /**< @brief x food position */
+#define FOOD_POS_Y            550 /**< @brief y food position */
 
-#define FOOD_MIN_X            480
-#define FOOD_MAX_X            552
-#define FOOD_MIN_Y            672
-#define FOOD_MAX_Y            742
+#define CHOOSE_RUDOLPH_MIN_X  350 /**< @brief x minimum position to click on rudolph */
+#define CHOOSE_RUDOLPH_MAX_X  675 /**< @brief x maximum position to click on rudolph */
+#define CHOOSE_RUDOLPH_MIN_Y  235 /**< @brief y minimum position to click on rudolph */
+#define CHOOSE_RUDOLPH_MAX_Y  535 /**< @brief y maximum position to click on rudolph */
 
-#define PLAY_MIN_X            693
-#define PLAY_MAX_X            768
-#define PLAY_MIN_Y            672
-#define PLAY_MAX_Y            742
+#define SLEEP_MIN_X           259 /**< @brief x minimum position to click on sleep button */
+#define SLEEP_MAX_X           331 /**< @brief x maximum position to click on sleep button */
+#define SLEEP_MIN_Y           672 /**< @brief y minimum position to click on sleep button */
+#define SLEEP_MAX_Y           742 /**< @brief y maximum position to click on sleep button */
 
-#define X_BARS                650
-#define Y_SLEEP_BAR           115
-#define Y_FOOD_BAR            175
-#define Y_PLAY_BAR            245
+#define FOOD_MIN_X            480 /**< @brief x minimum position to click on eat button */
+#define FOOD_MAX_X            552 /**< @brief x maximum position to click on eat button */
+#define FOOD_MIN_Y            672 /**< @brief y minimum position to click on eat button */
+#define FOOD_MAX_Y            742 /**< @brief y maximum position to click on eat button */
 
-#define X_COUNTER             228
-#define Y_COUNTER             0
+#define PLAY_MIN_X            693 /**< @brief x minimum position to click on the minigame button */
+#define PLAY_MAX_X            768 /**< @brief x maximum position to click on the minigame button */
+#define PLAY_MIN_Y            672 /**< @brief y minimum position to click on the minigame button */
+#define PLAY_MAX_Y            742 /**< @brief y maximum position to click on the minigame button */
 
-#define CLOUD_LEFT            123
-#define CLOUD_BOTTOM          499
-#define CLOUD_CENTER          373
-#define CLOUD_MIDDLE          364
-#define CLOUD_RIGHT           637
-#define CLOUD_TOP             231
+#define X_BARS                650 /**< @brief x position of the life bars */
+#define Y_SLEEP_BAR           115 /**< @brief y position of the sleep bar */
+#define Y_FOOD_BAR            175 /**< @brief y position of the eat bars */
+#define Y_PLAY_BAR            245 /**< @brief y position of the fun bars */
+
+#define X_COUNTER             228 /**< @brief x position counter of the minigame */
+#define Y_COUNTER             0   /**< @brief y position counter of the minigame */
+
+#define CLOUD_LEFT            123 /**< @brief x position of the left cloud */
+#define CLOUD_BOTTOM          499 /**< @brief y position of the bottom cloud */
+#define CLOUD_CENTER          373 /**< @brief x position of the center cloud */
+#define CLOUD_MIDDLE          364 /**< @brief y position of the middle cloud */
+#define CLOUD_RIGHT           637 /**< @brief x position of the right cloud */
+#define CLOUD_TOP             231 /**< @brief y position of the top cloud */
 
 /*************RTC****************/
 
-#define RTC_IRQ                  8 //The RTC has only one IRQ line, which is connected to line IRQ0 of PIC2, i.e. IRQ8
-#define RTC_ADDR_REG            0x70 //must be loaded with the address of the RTC register to be accessed
-#define RTC_DATA_REG            0x71 //used to transfer the data to/from the RTC's register accessed
+#define RTC_IRQ                  8 /**< @brief RTC IRQ */ 
+//The RTC has only one IRQ line, which is connected to line IRQ0 of PIC2, i.e. IRQ8  
+#define RTC_ADDR_REG            0x70 /**< @brief RTC address register*/ 
+//must be loaded with the address of the RTC register to be accessed 
+#define RTC_DATA_REG            0x71 /**< @brief RTC data register*/ 
+//used to transfer the data to/from the RTC's register accessed 
 
 #define REG_A                   0x0A
 #define REG_B                   0x0B
